394fc85bf16f807f2b4d50355e44774b0c24f550
denitsanp <denitsa_petkova@ue-varna.bg>
2024-01-04 22:56:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ function fetchSeatGeekData(query, isEventId = false) {
         url = `https://api.seatgeek.com/2/events?q=${query}&client_id=${seatGeekClientId}`;
     }
 
-    // return fetch(url)
-    //     .then(response => {
-    //         if (!response.ok) {
-    //             throw new Error(`HTTP error! Status: ${response.status}`);
-    //         }
-    //         return response.json();
-    //     })
-    //     .catch(error => {
-    //         console.error("Network error:", error);
-    //     });
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .catch(error => {
+            console.error("Network error:", error);
+        });
 }
 
 const searchBtn = document.getElementById('search-btn'); 
